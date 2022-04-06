a6cc45af4cdc9d302e1eb5e24b0191afb5ae6cd5
GustavoMenezes97 <gustavo.a.r.menezes@gmail.com>
2022-04-06 19:10:56 -0300

    Etapa 2 do exercicio

--- bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-primeiros-passos/script.js
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-primeiros-passos/script.js
@@ -1,14 +1,21 @@
+// Etapa 1
+
 const a = 29;
 const b = 23;
 
-
-
-
-
-
-// Etapa 1
 console.log(a + b);
 console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log(a % b);
+
+// Etapa 2
+
+const c = 12;
+const d = 32;
+
+if (c > d) {
+    console.log(c);
+} else {
+    console.log(d);
+}
