9e94982a2951263942b30b745dd89ef5ea12670d
GustavoMenezes97 <gustavo.a.r.menezes@gmail.com>
2022-04-08 18:17:49 -0300

    Etapa 6 do exercicio

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-primeiros-passos/script.js
@@ -48,7 +48,7 @@ if (h > 0) {
 
 const lado1 = 60;
 const lado2 = 60;
-const lado3 = 60;
+const lado3 = -61;
 
 let somaDosLados = lado1 + lado2 + lado3;
 let tudoPositivo = lado1 > 0 && lado2 > 0 && lado3 > 0;
@@ -63,3 +63,29 @@ if (somaDosLados === 180) {
     console.log("Erro: Valor negativo inserido");
 }
 
+// Etapa 6
+
+let movPecas = "RaInHa"
+
+switch (movPecas.toLowerCase()) {
+    case "peão":
+    console.log("O Peão move-se sempre uma casa para frente, exceto no primeiro movimento, quando pode mover-se duas casas.");
+    break;
+    case "torre":
+    console.log("A Torre move-se em linha reta, tanto na vertical quanto na horizontal, quantas casas quiser.");
+    break;
+    case "bispo":
+    console.log("O Bispo move-se na diagonal, quantas casas quiser.");
+    break;
+    case "cavalo":
+    console.log("O Cavalo move-se uma casa na horizontal e duas na vertical ou uma na vertial e duas na horizontal.");
+    break;
+    case "rainha":
+    console.log("A Rainha move-se em qualquer direção, quantas casas quiser.");
+    break;
+    case "rei":
+    console.log("O rei move-se em qualquer direção, apenas uma casa.")
+    break;
+    default:
+    console.log("Erro: Peça inválida.");
+}
