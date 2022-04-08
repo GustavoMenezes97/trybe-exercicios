9d206e9a2ecdcea89bc38c82fd8b167af1f75818
GustavoMenezes97 <gustavo.a.r.menezes@gmail.com>
2022-04-08 19:19:25 -0300

    Etapa 7 do exercicio

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-primeiros-passos/script.js
@@ -89,3 +89,24 @@ switch (movPecas.toLowerCase()) {
     default:
     console.log("Erro: Peça inválida.");
 }
+
+// Etapa 7
+
+const nota = 49;
+
+
+if (nota >= 90 && nota <= 100) {
+    console.log("Nota A");
+} else if (nota >= 80 && nota <= 89) {
+    console.log("Nota B");
+} else if (nota >= 70 && nota <= 79) {
+    console.log("Nota C");
+} else if (nota >= 60 && nota <= 69) {
+    console.log("Nota D");
+} else if (nota >= 50 && nota <= 59) {
+    console.log("Nota E");
+} else if (nota < 50 && nota >= 0) {
+    console.log("Nota F");
+} else {
+    console.log("Erro: Nota inválida.");
+}
