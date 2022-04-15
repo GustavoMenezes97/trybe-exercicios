1c5292b225041ff467f2b0464d47442333078aa0
GustavoMenezes97 <gustavo.a.r.menezes@gmail.com>
2022-04-15 20:17:59 -0300

    exercicio feito da 5 até a 9

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-for/script.js
@@ -1,4 +1,4 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 5, 19, 70, 8, 100, 2, 35, 27];
 
 for (let index = 0; index < numbers.length; index += 1) {
     // console.log(numbers[index]);
@@ -25,4 +25,50 @@ if (mediaAritmetica > 20) {
     // console.log('Valor maior que 20');
 } else {
     // console.log('Valor menor ou igual a 20');
+}
+
+let maiorNumero = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] > maiorNumero) {
+        maiorNumero = numbers[index];
+    }
+}
+
+    // console.log(maiorNumero);
+
+let quantidadeImpar = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 !== 0) {
+        quantidadeImpar += 1;
+    }
+}
+
+if (quantidadeImpar === 0) {
+    // console.log('nenhum valor ímpar encontrado');
+} else {
+    // console.log('Qtd números ímpares: ' + quantidadeImpar);
+}
+
+let menorNumero = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] < menorNumero) {
+        menorNumero = numbers[index];
+    }
+}
+
+    // console.log(menorNumero);
+
+let novoArray = [];
+
+for (let index = 1; index <= 25; index += 1) {
+    novoArray.push(index);
+}
+
+    // console.log(novoArray);
+
+for (let index = 0; index < novoArray.length; index += 1) {
+        // console.log(novoArray[index] / 2);
 }
