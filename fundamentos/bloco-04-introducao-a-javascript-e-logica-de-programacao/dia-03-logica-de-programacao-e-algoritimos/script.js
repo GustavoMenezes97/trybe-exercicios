e17544d4d7455b025ca64d816c454ff3f5e7606d
GustavoMenezes97 <gustavo.a.r.menezes@gmail.com>
2022-04-15 21:49:43 -0300

    exercicio 2 e 3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritimos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritimos/script.js
@@ -4,4 +4,34 @@ for (let index = 10; index > 0; index -= 1) {
     fatorialDeDez *= index;
 }
 
-    console.log(fatorialDeDez);
+// console.log(fatorialDeDez);
+
+let word = 'tryber';
+let inverseWord = '';
+
+for (let index = 0; index < word.length; index += 1) {
+    inverseWord += word[word.length - 1 - index];
+}
+
+// console.log(inverseWord);
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = '';
+
+for (index = 0; index < array.length; index += 1) {
+    if (array[index].length > maiorPalavra.length) {
+    maiorPalavra = array[index];
+    }
+}
+
+console.log(maiorPalavra);
+
+let menorPalavra = array[0];
+
+for (index = 0; index < array.length; index += 1) {
+    if (array[index].length < menorPalavra.length) {
+    menorPalavra = array[index];
+    }
+}
+
+console.log(menorPalavra);
